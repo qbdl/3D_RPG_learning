fc3b8abdf07c3fe572a27d96498637ea52728b2f
qbdl <1260017352@qq.com>
2025-06-05 13:07:06 +0800

    3DRPG游戏开发 Core核心功能 Course 07-09

--- Assets/Scripts/Characters/PlayerController.cs
+++ Assets/Scripts/Characters/PlayerController.cs
@@ -3,19 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-
 public class PlayerController : MonoBehaviour
 {
     private NavMeshAgent agent;
+    private Animator anim;
 
     void Awake()//在对象被加载时调用
     {
-        agent = GetComponent<NavMeshAgent>();
+        agent = GetComponent<NavMeshAgent>();//获取前端的NavMeshAgent
+        anim = GetComponent<Animator>();//获取前端的Animator
     }
 
     void Start()//在对象启用后、第一次更新帧之前调用
     {
         MouseManager.Instance.OnMouseClicked += MoveToTarget;
+    }
+
+    void Update()
+    {
+        SwitchAnimation(); //每帧更新动画状态
+    }
+
+    private void SwitchAnimation()
+    {
+        anim.SetFloat("Speed", agent.velocity.sqrMagnitude); //设置前端的动画速度参数
     }
 
     public void MoveToTarget(Vector3 target)
