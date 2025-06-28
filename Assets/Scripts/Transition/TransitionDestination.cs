267d21d3ae9b6383f20760f3a7b6db6c50437a44
qbdl <1260017352@qq.com>
2025-06-28 12:22:12 +0800

    修改地形并添加传送门,调整景深与Lighting

--- Assets/Scripts/Transition/TransitionDestination.cs
+++ Assets/Scripts/Transition/TransitionDestination.cs
@@ -4,7 +4,18 @@ using UnityEngine;
 
 public class TransitionDestination : MonoBehaviour
 {
-    public enum DestinationTag { ENTER, TREASURE, PEAK, DANGER, NEXT_LEVEL }
+    public enum DestinationTag
+    {
+        ENTER,
+        TREASURE,
+        MONSTER,
+        SECRET_AREA,
+        PEAK_GRAY, PEAK_RED,
+        PEAK_DRAGON_IN, PEAK_DRAGON_OUT,
+        PEAK_BOSS_IN, PEAK_BOSS_OUT,
+        DANGER,
+        NEXT_LEVEL
+    }
 
     public DestinationTag destinationTag; // 传送目的地标签
 }
